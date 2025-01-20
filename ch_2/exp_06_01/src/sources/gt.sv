a20336b93a27d06f7b56ddb26b3c5203ca2c75ed
Zhongyi Chen <zchen@ambiq.com>
2025-01-19 18:59:02 -0800

    exp 1.6.1 init

---
+++ ch_2/exp_06_01/src/sources/gt.sv
@@ -0,0 +1,21 @@
+module gt_sop
+    (
+        input logic [1:0] a, b,
+        output logic agtb
+    )
+
+    logic p0, p1, p2, p3, p4, p5;
+
+    // summation
+    assign agtb = p0 | p1 | p2 | p3 | p4 | p5;
+
+    // product terms
+    assign p0 = (~b[1] & ~b[0]) & (~a[1] &  a[0]); // 00 < 01
+    assign p1 = (~b[1] & ~b[0]) & ( a[1] & ~a[0]); // 00 < 10
+    assign p2 = (~b[1] & ~b[0]) & ( a[1] &  a[0]); // 00 < 11
+    assign p3 = (~b[1] &  b[0]) & ( a[1] & ~a[0]); // 01 < 10
+    assign p4 = (~b[1] &  b[0]) & ( a[1] &  a[0]); // 01 < 11
+    assign p5 = ( b[1] & ~b[0]) & ( a[1] &  a[0]); // 10 < 11
+    
+
+endmodule
