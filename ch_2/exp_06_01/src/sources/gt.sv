08945aa472b60f87107d29af6ad18426587407be
kchen3157 <grubby-choral-0a@icloud.com>
2025-01-20 21:32:57 -0800

    add 4 bit version of gt comparator

--- ch_2/exp_06_01/src/sources/gt.sv
+++ ch_2/exp_06_01/src/sources/gt.sv
@@ -2,7 +2,7 @@ module gt_2_sop
     (
         input logic [1:0] a, b,
         output logic agtb
-    )
+    );
 
     logic p0, p1, p2, p3, p4, p5;
 
@@ -18,5 +18,24 @@ module gt_2_sop
     assign p4 = (~b[1] &  b[0]) & ( a[1] &  a[0]); // 01 < 11
     assign p5 = ( b[1] & ~b[0]) & ( a[1] &  a[0]); // 10 < 11
     
+
+endmodule
+
+
+module gt_4_sop
+    (
+        input logic [3:0] a, b,
+        output logic agtb
+    );
+
+    logic agtb_low, agtb_high;
+
+    gt_2_sop gt_2_sop_inst_low
+        (.a(a[0:1]), .b(b[0:1]), .agtb(agtb_low));
+    
+    gt_2_sop gt_2_sop_inst_high
+        (.a(a[2:3]), .b(b[2:3]), .agtb(agtb_high));
+
+    assign agtb = agtb_high | (agtb_low & ~agtb_high)
 
 endmodule
