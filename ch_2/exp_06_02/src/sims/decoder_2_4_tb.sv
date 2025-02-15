bec7d5d19ef50b4de85f2d93b907d357d5bbe892
kchen3157 <grubby-choral-0a@icloud.com>
2025-02-14 20:39:42 -0800

    derive tb for decoder_2_4

---
+++ ch_2/exp_06_02/src/sims/decoder_2_4_tb.sv
@@ -0,0 +1,41 @@
+`timescale 1 ns/10 ps
+
+module decoder_2_4_testbench
+    
+    logic test_en_in;
+    logic [1:0] test_a_in;
+    logic [3:0] test_bcode_out;
+
+    decoder_2_4 uut
+        (.en(test_en_in), .a(test_a_in), .bcode(test_bcode_out));
+    initial
+    begin
+        // EN
+        test_en_in = 1'b1;
+
+        test_a_in = 2'b00;
+        # 200;
+        test_a_in = 2'b01;
+        # 200;
+        test_a_in = 2'b10;
+        # 200;
+        test_a_in = 2'b11;
+        # 200;
+
+
+        // !EN
+        test_en_in = 1'b0;
+
+        test_a_in = 2'b00;
+        # 200;
+        test_a_in = 2'b01;
+        # 200;
+        test_a_in = 2'b10;
+        # 200;
+        test_a_in = 2'b11;
+        # 200;
+
+        // stop sim
+        $stop;
+    end
+endmodule
