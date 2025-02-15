4c697c03bf24786b7e3293a00bea411e1b4c0b6b
kchen3157 <grubby-choral-0a@icloud.com>
2025-02-14 21:00:11 -0800

    decoder_2_4 and build working

--- ch_2/exp_06_02/src/sources/top.sv
+++ ch_2/exp_06_02/src/sources/top.sv
@@ -1,7 +1,14 @@
 module top
     (
-        
+        input logic [1:0] sw,
+        output logic [3:0] led
     );
 
+    wire en_w;
+
+    assign en_w = 1'b1;
+
+    decoder_2_4 decoder_2_4_inst
+        (.en(en_w), .a(sw), .bcode(led));
 
 endmodule
