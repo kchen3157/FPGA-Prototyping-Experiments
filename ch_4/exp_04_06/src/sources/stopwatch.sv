59bb5cd5ad38b2b70354fa847b1e502c5b6bbc2a
kchen3157 <grubby-choral-0a@icloud.com>
2025-07-10 23:42:22 -0700

    exp_04_06: up/down working

--- ch_4/exp_04_06/src/sources/stopwatch.sv
+++ ch_4/exp_04_06/src/sources/stopwatch.sv
@@ -3,7 +3,7 @@ module stopwatch_cascade
     #(parameter DVSR = 10_000_000)
     (
         input   logic i_clk,
-        input   logic i_go, i_clr,
+        input   logic i_go, i_clr, i_up,
         output  logic [3:0] o_s3, o_s2, o_s1, o_s0
     );
 
@@ -29,27 +29,65 @@ module stopwatch_cascade
     assign w_ms_tick = (r_ms == DVSR) ? 1'b1 : 1'b0;
 
     assign w_s0_en = w_ms_tick;
-    assign w_s0_next = (i_clr || (w_s0_en && r_s0 == 9)) ? 4'b0 :
-                       (w_s0_en) ? r_s0 + 1 :
+    assign w_s0_next = (i_clr || (w_s0_en && r_s0 == 9 && i_up)) ? 4'b0 :
+                       (w_s0_en && r_s0 == 4'h0 && ~i_up) ? 4'h9 :
+                       (w_s0_en && i_up) ? r_s0 + 1 :
+                       (w_s0_en) ? r_s0 - 1 :
                        r_s0;
-    assign w_s0_tick = (r_s0 == 9) ? 1'b1 : 1'b0;
+    assign w_s0_tick = (r_s0 == 4'h9 && i_up) ? 1'b1 :
+                       (r_s0 == 4'h0 && ~i_up) ? 1'b1 :
+                       1'b0;
 
     assign w_s1_en = w_ms_tick & w_s0_tick;
-    assign w_s1_next = (i_clr || (w_s1_en && r_s1 == 9)) ? 4'b0 :
-                       (w_s1_en) ? r_s1 + 1 :
+    assign w_s1_next = (i_clr || (w_s1_en && r_s1 == 9 && i_up)) ? 4'b0 :
+                       (w_s1_en && r_s1 == 4'h0 && ~i_up) ? 4'h9 :
+                       (w_s1_en && i_up) ? r_s1 + 1 :
+                       (w_s1_en) ? r_s1 - 1 :
                        r_s1;
-    assign w_s1_tick = (r_s1 == 9) ? 1'b1 : 1'b0;
+    assign w_s1_tick = (r_s1 == 4'h9 && i_up) ? 1'b1 :
+                       (r_s1 == 4'h0 && ~i_up) ? 1'b1 :
+                       1'b0;
 
     assign w_s2_en = w_ms_tick & w_s0_tick & w_s1_tick;
-    assign w_s2_next = (i_clr || (w_s2_en && r_s2 == 5)) ? 4'b0 :
-                       (w_s2_en) ? r_s2 + 1 :
+    assign w_s2_next = (i_clr || (w_s2_en && r_s2 == 5 && i_up)) ? 4'b0 :
+                       (w_s2_en && r_s2 == 4'h0 && ~i_up) ? 4'h5 :
+                       (w_s2_en && i_up) ? r_s2 + 1 :
+                       (w_s2_en) ? r_s2 - 1 :
                        r_s2;
-    assign w_s2_tick = (r_s2 == 5) ? 1'b1 : 1'b0;
+    assign w_s2_tick = (r_s2 == 4'h5 && i_up) ? 1'b1 :
+                       (r_s2 == 4'h0 && ~i_up) ? 1'b1 :
+                       1'b0;
 
     assign w_s3_en = w_ms_tick & w_s0_tick & w_s1_tick & w_s2_tick;
-    assign w_s3_next = (i_clr || (w_s3_en && r_s3 == 9)) ? 4'b0 :
-                       (w_s3_en) ? r_s3 + 1 :
+    assign w_s3_next = (i_clr || (w_s3_en && r_s3 == 9 && i_up)) ? 4'b0 :
+                       (w_s3_en && r_s3 == 4'h0 && ~i_up) ? 4'h9 :
+                       (w_s3_en && i_up) ? r_s3 + 1 :
+                       (w_s3_en) ? r_s3 - 1 :
                        r_s3;
+
+
+    // assign w_s0_en = w_ms_tick;
+    // assign w_s0_next = (i_clr || (w_s0_en && r_s0 == 4'h0)) ? 4'h9 :
+    //                    (w_s0_en) ? r_s0 - 1 :
+    //                    r_s0;
+    // assign w_s0_tick = (r_s0 == 0) ? 1'b1 : 1'b0;
+
+    // assign w_s1_en = w_ms_tick & w_s0_tick;
+    // assign w_s1_next = (i_clr || (w_s1_en && r_s1 == 4'h0)) ? 4'h9 :
+    //                    (w_s1_en) ? r_s1 - 1 :
+    //                    r_s1;
+    // assign w_s1_tick = (r_s1 == 0) ? 1'b1 : 1'b0;
+
+    // assign w_s2_en = w_ms_tick & w_s0_tick & w_s1_tick;
+    // assign w_s2_next = (i_clr || (w_s2_en && r_s2 == 4'h0)) ? 4'h5 :
+    //                    (w_s2_en) ? r_s2 - 1 :
+    //                    r_s2;
+    // assign w_s2_tick = (r_s2 == 0) ? 1'b1 : 1'b0;
+
+    // assign w_s3_en = w_ms_tick & w_s0_tick & w_s1_tick & w_s2_tick;
+    // assign w_s3_next = (i_clr || (w_s3_en && r_s3 == 4'h0)) ? 4'h9 :
+    //                    (w_s3_en) ? r_s3 - 1 :
+    //                    r_s3;
 
     assign o_s0 = r_s0;
     assign o_s1 = r_s1;
--- ch_4/exp_04_06/src/sources/top.sv
+++ ch_4/exp_04_06/src/sources/top.sv
@@ -35,7 +35,7 @@
 module top
     (
         input   logic clk, cpu_resetn,
-        input   logic sw,
+        input   logic [1:0] sw,
         output  logic [1:0] set_vadj,
         output  logic vadj_en,
         output  logic fmc_clk0_m2c_n,
@@ -77,8 +77,8 @@ module top
     logic [7:0] w_led_map [3:0];
 
     stopwatch_cascade u_stopwatch_cascade
-        (.i_clk(clk), .i_go(sw), .i_clr(~cpu_resetn), .o_s3(w_s3),
-         .o_s2(w_s2), .o_s1(w_s1), .o_s0(w_s0));
+        (.i_clk(clk), .i_go(sw[0]), .i_up(sw[1]), .i_clr(~cpu_resetn),
+         .o_s3(w_s3), .o_s2(w_s2), .o_s1(w_s1), .o_s0(w_s0));
         
     hex_to_sseg u_hex_to_sseg_LD1
         (.i_hex(w_s0), .i_dp(1'b0), .o_sseg_n(w_led_map[0]));
@@ -93,6 +93,7 @@ module top
         (.i_hex(w_s3), .i_dp(1'b1), .o_sseg_n(w_led_map[3]));
 
     led_4_1_mux u_led_4_1_mux
-        (.i_clk(w_clk_slow), .i_reset(~cpu_resetn), .i_in_n(w_led_map), .o_ldsel(w_ldsel), .o_sseg_n(w_sseg_n));
+        (.i_clk(w_clk_slow), .i_reset(~cpu_resetn), .i_in_n(w_led_map),
+         .o_ldsel(w_ldsel), .o_sseg_n(w_sseg_n));
 
 endmodule
