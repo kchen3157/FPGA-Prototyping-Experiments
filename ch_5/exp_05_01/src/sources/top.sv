693023bfb1e0c62d8936888346ffb434a8fc6801
kchen3157 <grubby-choral-0a@icloud.com>
2025-07-22 11:11:56 -0700

    exp_05_01: add testpoints

--- ch_5/exp_05_01/src/sources/top.sv
+++ ch_5/exp_05_01/src/sources/top.sv
@@ -2,16 +2,29 @@ module top
     (
         input   logic clk, cpu_resetn,
         input   logic sw,
-        output  logic ja0
+        output  logic ja0, ja1, ja2
     );
 
+    // generate slower (default 10ms) tick
+    logic w_slow_tick;
+    logic [3:0] r_tick_counter, w_tick_counter_next;
+    always_ff @(posedge clk)
+    begin
+        r_tick_counter <= w_tick_counter_next;
+    end
+    assign w_tick_counter_next = r_tick_counter + 1;
+    assign w_slow_tick = (r_tick_counter == 0) ? 1'b1 : 1'b0;
+    assign ja0 = w_slow_tick;
+
     // dualedge_detector_moore u_dualedge_detector_moore
-    //     (.i_clk(clk), .i_rst(~cpu_resetn), .i_lvl(sw),
-    //      .o_edge(ja0));
+    //     (.i_clk(w_slow_tick), .i_rst(~cpu_resetn), .i_lvl(sw),
+    //      .o_edge(ja1));
     
     dualedge_detector_mealy u_dualedge_detector_mealy
-        (.i_clk(clk), .i_rst(~cpu_resetn), .i_lvl(sw),
-         .o_edge(ja0));
+        (.i_clk(w_slow_tick), .i_rst(~cpu_resetn), .i_lvl(sw),
+         .o_edge(ja1));
+
+    assign ja2 = sw;
 
 
 endmodule
