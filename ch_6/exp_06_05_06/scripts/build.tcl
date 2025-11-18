b7acb2714b17beca3a18a69158b2bed1a651e506
kchen3157 <grubby-choral-0a@icloud.com>
2025-11-17 17:50:56 -0800

    exp_06_05_06: builds and seems to work correctly

--- ch_6/exp_06_05_06/scripts/build.tcl
+++ ch_6/exp_06_05_06/scripts/build.tcl
@@ -1,9 +1,15 @@
 ######## SET DESIGN SOURCES HERE (ONE PER FILE) ########
 read_verilog -sv "src/sources/top.sv"
+read_verilog -sv "src/sources/reaction.sv"
 read_verilog -sv "src/sources/sseg4/bintobcd.sv"
 read_verilog -sv "src/sources/sseg4/hextosseg.sv"
 read_verilog -sv "src/sources/sseg4/sseg4.sv"
 read_verilog -sv "src/sources/sseg4/ledmux.sv"
+read_verilog -sv "src/sources/rand_gen/div.sv"
+read_verilog -sv "src/sources/rand_gen/lfsr32.sv"
+read_verilog -sv "src/sources/rand_gen/rand_gen.sv"
+
+
 
 
 ######## SET CONSTRAINTS HERE ########
--- ch_6/exp_06_05_06/src/sources/top.sv
+++ ch_6/exp_06_05_06/src/sources/top.sv
@@ -1,9 +1,12 @@
 module top
     (
         input   logic clk, cpu_resetn,
-        input   logic btnc,
+        input   logic btnl, // START
+        input   logic btnc, // STOP
+        input   logic btnr, // CLEAR
 
-        input   logic [7:0] sw,
+        output   logic [7:0] led,
+
 
         // FMC sseg out
         output  logic fmc_clk0_m2c_n, fmc_clk1_m2c_n,
@@ -17,6 +20,9 @@ module top
     );
 
     //********* DISPLAY OUTPUT *********
+    // Create nets to connect sseg4 and reaction together
+    logic [13:0] w_reaction_val;
+    logic w_reaction_greeting, w_reaction_led;
     // Create sseg/ldsel nets
     logic [7:0] w_sseg_n;
     logic [3:0] w_ldsel;
@@ -31,12 +37,28 @@ module top
     sseg4 u_sseg4
     (
         .i_clk(clk), .i_rst(~cpu_resetn),
-        .i_bin({6'h00, sw}),
-        .i_greeting(btnc),               // When asserted, display "HI"
+        .i_bin(w_reaction_val),
+        .i_greeting(w_reaction_greeting),               // When asserted, display "HI"
 
         .o_sseg_n(w_sseg_n),
         .o_ldsel(w_ldsel),
         .o_idle()                    // For siulation usage
     );
+
+    
+    reaction u_reaction
+    (
+        .i_clk(clk), .i_rst(~cpu_resetn),
+
+        .i_start(btnl), .i_clear(btnr), .i_stop(btnc), // user control signals
+
+        .o_display_val(w_reaction_val), // output to display, in binary (BCD conversion and led mux handled by other modules)
+        .o_display_greeting(w_reaction_greeting), // when high, the display module ignores input val and displays "HI"
+        .o_led(w_reaction_led) // stimulus LED
+    );
+
+    assign led = (w_reaction_led) ? 8'b11111111 : 8'b00000000;
+
+
 
 endmodule
