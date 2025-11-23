97d396f60dec543ab2ba97a679dbcb0a3c57b1db
kchen3157 <grubby-choral-0a@icloud.com>
2025-11-23 11:58:48 -0800

    exp_07_07_03: init

---
+++ ch_7/exp_07_07_03/readme.md
@@ -0,0 +1,3 @@
+# 7.7.3 FIFO with Data Width Conversion
+
+This is a FWFT FIFO with a 16-bit write port and 8-bit read port. It is designed with a 
---
+++ ch_7/exp_07_07_03/scripts/build.tcl
@@ -0,0 +1,18 @@
+######## SET DESIGN SOURCES HERE (ONE PER FILE) ########
+read_verilog -sv "src/sources/top.sv"
+
+######## SET CONSTRAINTS HERE ########
+read_xdc "src/constrs/Nexys-Video-Master.xdc"
+
+######## SET PART HERE ########
+synth_design -top "top" -part "xc7a200tsbg484-1"
+
+# place and route
+opt_design
+place_design
+route_design
+
+report_utilization -file utilizationReport.txt
+
+######## SET FINAL BITSTREAM OUTPUT HERE ########
+write_bitstream -force "build/proj.bit"
